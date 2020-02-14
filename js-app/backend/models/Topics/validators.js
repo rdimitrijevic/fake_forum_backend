19727d648e062e534a2193ace47aa2f6e1578499
rdimitrijevic <rdimitrijevic06@gmail.com>
2020-02-14 02:31:58 +0100

    Minimalne izmene u strukturi projekta

---
+++ js-app/backend/models/Topics/validators.js
@@ -0,0 +1,14 @@
+
+function topicTitle(name) {
+    const regex = /[\w ()\[\].,{}$&*<>@!%"':-]/gu;
+    return regex.test(name);
+}
+
+const titleVal = {
+    validator: topicTitle,
+    message: "Title contains forbiden characters"
+}
+
+module.exports = {
+    titleVal
+}
